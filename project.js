91d24a5b9953ee88a3d3f230dab46c199f40dd17
Laura-Hassert <laura.hassert@gmail.com>
2021-10-11 15:42:21 -0400

    added more tests

--- project.js
+++ project.js
@@ -2,5 +2,11 @@ module.exports = {
 
     getGreeting: (name) => `Hi ${name}`,
 
-    sumNums: (num1, num2) => num1 + num2
+    sumNums: (num1, num2) => num1 + num2,
+
+    subtractNums: (num1, num2) => num1 - num2,
+
+    multiplyNums: (num1, num2) => num1 * num2,
+    
+    divideNums: (num1, num2) => num1 / num2
 };
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { getGreeting, sumNums } = require('./project');
+const { getGreeting, sumNums, subtractNums, multiplyNums, divideNums } = require('./project');
 
 test('can create a basic greeting', () => {
   expect(getGreeting("Sam")).toBe("Hi Sam");
@@ -6,4 +6,16 @@ test('can create a basic greeting', () => {
 
 test('adds two numbers together', () => {
   expect(sumNums(3, 7)).toBe(10);
+});
+
+test('subtracts two numbers', () => {
+  expect(subtractNums(10, 3)).toBe(7);
+});
+
+test('multiply two numbers together', () => {
+  expect(multiplyNums(4, 5)).toBe(20);
+});
+
+test('divide two numbers', () => {
+  expect(divideNums(10, 5)).toBe(2);
 });
